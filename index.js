b5690080a47564e8ef9fe30215779a5fc58ea82d
Nitesh Yadav <niteshyadav604@gmail.com>
2026-02-24 19:12:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+function countUniqueValues(arr) {
+  // Counts unique values in a sorted array using two-pointer technique
+  if (!Array.isArray(arr) || arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+
+function anagramString(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let lookup = {};
+  for (let char of str1) {
+    let letter = char;
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  for (let char of str2) {
+    let letter = char;
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+console.log(anagramString("niiit", "ntiii"));
+
+function sameFrequency(n1,n2){
+  // good luck. Add any arguments you deem necessary.
+  // let arr1=[]
+  // let arr2=[]
+  // while(n1>0 && n2>0){
+  //     let m= Math.floor(n1%10);
+  //     console.log(m)
+  //     let m1=Math.floor(n2%10);
+  //     arr1.push(m);
+  //     arr2.push(m1)
+  //     n1=Math.floor(n1/10); n2=Math.floor(n2/10)
+  // }
+  // console.log(arr1,arr2)
+  // for(let val of arr1){
+  //     if(arr2.indexOf(val)===-1){
+  //         return false
+  //     }
+  //     arr2.splice(arr2.indexOf(val),1)
+  // }
+  // return true
+
+
+  n1 = n1.toString();
+  n2 = n2.toString();
+  console.log(n1,n2)
+  if (n1.length !== n2.length) {
+    return false;
+  }
+ let lookup = {};
+ for(let char of n1){
+  console.log(char)
+   let letter = char;
+   lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+ }
+  for(let char of n2){
+    let letter = char;
+    if(!lookup[letter]){
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+
+  
+}
+console.log(sameFrequency(182,281))
+
+
+function zeroSumPair(arr){
+  let left =0
+  let right = arr.length-1;
+  while(left<right){
+    let sum = arr[left]+arr[right];
+    if(sum===0){
+      return [arr[left],arr[right]]
+    }else if(sum>0){
+      right --;
+    }else{
+      left++;
+    }
+  }
+}
+console.log(zeroSumPair([-3,-2,-1,0,1,2,3]))
--- reverseString.js
+++ reverseString.js
@@ -40,3 +40,57 @@ console.log(secondWayToAdd(num));
 // solve / simplify
 // look back and refactor always in the code 
 
+
+// write a function 
+
+let arr1 = [1,2,4,3];
+let arr2 = [9,16,1,4];
+
+function same(arr1,arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+
+    }
+    for(let i = 0; i < arr1.length ; i++){
+        let correctIndex=arr2.indexOf(arr1[i]**2)
+        if(correctIndex === -1){
+            return 0;
+        }
+        arr2.splice(correctIndex,1)
+        console.log(arr2)
+    }
+    return 1;
+}
+console.log(same(arr1,arr2))
+
+
+
+let s1="niteshh";
+let s2="tehsinh";
+
+function validAnagram(s1,s2){   
+    if(s1.length !== s2.length){
+        return false;
+    }
+    let f1={};
+    let f2={};
+    for(let char of s1){
+        f1[char] = (f1[char] || 0) + 1;
+    }
+    for(let char of s2){
+        f2[char] = (f2[char] || 0) + 1;
+    }
+    console.log(f1)
+    console.log(f2)
+    for(let key in f1){
+        if(f1[key] !== f2[key]){
+            return false;
+        }
+        if(!(key in f2)){
+            return false;
+        }
+
+    }
+    return true;
+}
+console.log(validAnagram(s1,s2))
